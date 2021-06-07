1f89082badd858124dbbc2082fdc40f3271a50b5
Zinal <Zinal001@users.noreply.github.com>
2021-06-07 09:42:00 +0200

    Misc Commands: - Items are now loaded and cached as soon as possible, this is to prevent the game from lagging up when using the /give command.

--- MiscCommands/MiscCommandsMod.cs
+++ MiscCommands/MiscCommandsMod.cs
@@ -8,11 +8,19 @@ namespace MiscCommands
     [BepInDependency("tech.zinals.plugins.chatcommands", BepInDependency.DependencyFlags.HardDependency)]
     public class MiscCommandsMod : BaseUnityPlugin
     {
+        internal static BepInEx.Logging.ManualLogSource ModLogger { get; private set; }
 
         public MiscCommandsMod()
         {
+            ModLogger = Logger;
+
             HarmonyLib.Harmony.CreateAndPatchAll(typeof(Patches));
             ChatCommands.ChatSystem.OverwriteAll(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+
+        public void Awake()
+        {
+            StartCoroutine(Utils.LoadAllItems());
         }
 
     }
--- MiscCommands/Utils.cs
+++ MiscCommands/Utils.cs
@@ -7,8 +7,13 @@ namespace MiscCommands
 {
     internal static class Utils
     {
-        internal static Game.EntityFramework.CraftableObject[] GetAllItems()
+        private static Game.EntityFramework.CraftableObject[] _AllItems = null;
+
+        internal static System.Collections.IEnumerator LoadAllItems()
         {
+            while (!Game.Crafting.CraftingMenu.InstanceExists)
+                yield return new UnityEngine.WaitForSeconds(1f);
+
             var allItems = UnityEngine.Resources.FindObjectsOfTypeAll<Game.EntityFramework.CraftableObject>();
             List<Game.EntityFramework.CraftableObject> items = new List<Game.EntityFramework.CraftableObject>();
 
@@ -17,6 +22,8 @@ namespace MiscCommands
                 if (!items.Any(i => i.pPrefabAssetPath == item.pPrefabAssetPath && i.pEntityName == item.pEntityName))
                     items.Add(item);
             }
+
+            yield return new UnityEngine.WaitForEndOfFrame();
 
             if (Game.Crafting.CraftingMenu.InstanceExists)
             {
@@ -30,8 +37,10 @@ namespace MiscCommands
 
                 if (boxRectToAssetPath != null)
                 {
+                    int pathsRan = 0;
                     foreach (var pair in boxRectToAssetPath)
                     {
+                        pathsRan++;
                         if (!visitedAssetPaths.Contains(pair.Value))
                         {
                             visitedAssetPaths.Add(pair.Value);
@@ -40,18 +49,74 @@ namespace MiscCommands
 
                             Game.EntityFramework.CraftableObject component = UnityEngine.Resources.Load<UnityEngine.GameObject>(pair.Value).GetComponent<Game.EntityFramework.CraftableObject>();
                             if (component == null)
-                                UnityEngine.Debug.LogWarning($"Failed to load object '{pair.Value}'");
+                                MiscCommandsMod.ModLogger.LogWarning($"Failed to load object '{pair.Value}'");
                             else
                             {
                                 if (!items.Any(i => i.pPrefabAssetPath == component.pPrefabAssetPath && i.pEntityName == component.pEntityName))
                                     items.Add(component);
                             }
                         }
+
+                        if (pathsRan % 5 == 0)
+                            yield return new UnityEngine.WaitForEndOfFrame();
                     }
                 }
             }
 
-            return items.ToArray();
+            _AllItems = items.ToArray();
+            MiscCommandsMod.ModLogger.LogInfo("All items loaded");
+        }
+
+        internal static Game.EntityFramework.CraftableObject[] GetAllItems()
+        {
+            if(_AllItems == null)
+            {
+                var allItems = UnityEngine.Resources.FindObjectsOfTypeAll<Game.EntityFramework.CraftableObject>();
+                List<Game.EntityFramework.CraftableObject> items = new List<Game.EntityFramework.CraftableObject>();
+
+                foreach (var item in allItems)
+                {
+                    if (!items.Any(i => i.pPrefabAssetPath == item.pPrefabAssetPath && i.pEntityName == item.pEntityName))
+                        items.Add(item);
+                }
+
+                if (Game.Crafting.CraftingMenu.InstanceExists)
+                {
+                    System.Reflection.FieldInfo _CraftingMenuBoxRectsField = typeof(Game.Crafting.CraftingMenu).GetField("boxRects", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                    System.Reflection.FieldInfo _CraftingMenuBoxRectToAssetPathField = typeof(Game.Crafting.CraftingMenu).GetField("boxRectToAssetPath", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+                    List<UnityEngine.RectTransform> boxRects = (List<UnityEngine.RectTransform>)_CraftingMenuBoxRectsField.GetValue(Game.Crafting.CraftingMenu.Instance);
+                    Dictionary<UnityEngine.RectTransform, String> boxRectToAssetPath = (Dictionary<UnityEngine.RectTransform, String>)_CraftingMenuBoxRectToAssetPathField.GetValue(Game.Crafting.CraftingMenu.Instance);
+
+                    List<String> visitedAssetPaths = new List<String>();
+
+                    if (boxRectToAssetPath != null)
+                    {
+                        foreach (var pair in boxRectToAssetPath)
+                        {
+                            if (!visitedAssetPaths.Contains(pair.Value))
+                            {
+                                visitedAssetPaths.Add(pair.Value);
+                                if (!Game.InventoryFramework.Inventory.Instance.IsAssetPathValid(pair.Value))
+                                    continue;
+
+                                Game.EntityFramework.CraftableObject component = UnityEngine.Resources.Load<UnityEngine.GameObject>(pair.Value).GetComponent<Game.EntityFramework.CraftableObject>();
+                                if (component == null)
+                                    MiscCommandsMod.ModLogger.LogWarning($"Failed to load object '{pair.Value}'");
+                                else
+                                {
+                                    if (!items.Any(i => i.pPrefabAssetPath == component.pPrefabAssetPath && i.pEntityName == component.pEntityName))
+                                        items.Add(component);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                _AllItems = items.ToArray();
+            }
+
+            return _AllItems;
         }
     }
 }
